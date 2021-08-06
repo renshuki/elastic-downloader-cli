7b71f0a3c40e3bfb17ed8a1edae7b1213f6af6f3
renshuki <renshuki@users.noreply.github.com>
2021-08-07 00:25:03 +0900

    Implement product versions fetch from Github API

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 #!/usr/bin/env node
 
-const figlet = require("figlet");
+const figlet = require('figlet');
 const chalk = require('chalk');
 const axios = require('axios');
 const inquirer = require('inquirer');
+inquirer.registerPrompt('search-list', require('inquirer-search-list'));
 
 console.log(
     chalk.green(
         figlet.textSync('Elastic Downloader CLI', { horizontalLayout: 'default' })
         )
     );
-
 
 inquirer.prompt([
 {
@@ -19,16 +19,16 @@ inquirer.prompt([
     message: 'Select an architecture:',
     choices: [
         {
-            name: "LINUX X86-64",
-            value: "linux-x86_64"
+            name: 'LINUX X86-64',
+            value: 'linux-x86_64'
         },
         {
-            name: "MACOS",
-            value: "darwin-x86_64"
+            name: 'MACOS',
+            value: 'darwin-x86_64'
         },
         {
-            name: "WINDOWS",
-            value: "windows-x86_64"
+            name: 'WINDOWS',
+            value: 'windows-x86_64'
         },
     ]
 },
@@ -38,82 +38,91 @@ inquirer.prompt([
     message: 'Select a product:',
     pageSize: 20,
     choices: [
-        new inquirer.Separator("--- Elastic Stack ---"),
+        new inquirer.Separator('--- Elastic Stack ---'),
         {
-            name: "Elasticsearch",
-            value: "elasticsearch"
+            name: 'Elasticsearch',
+            value: 'elasticsearch'
         },
         {
-            name: "Kibana",
-            value: "kibana"
+            name: 'Kibana',
+            value: 'kibana'
         },
         {
-            name: "Logstash",
-            value: "logstash"
+            name: 'Logstash',
+            value: 'logstash'
         },
-        new inquirer.Separator("--- Elastic Enterprise Search ---"),
+        new inquirer.Separator('--- Elastic Enterprise Search ---'),
         {
-            name: "App Search",
-            value: "app-search"
-        },
-        new inquirer.Separator("--- Elastic Observability ---"),
-        {
-            name: "APM Server",
-            value: "apm-server"
+            name: 'App Search',
+            value: 'app-search'
         },
         {
-            name: "Elastic Agent",
-            value: "elastic-agent"
+            name: 'Enterprise Search',
+            value: 'enterprise-search'
+        },
+        new inquirer.Separator('--- Elastic Observability ---'),
+        {
+            name: 'APM Server',
+            value: 'apm-server'
         },
         {
-            name: "Auditbeat",
-            value: "auditbeat"
+            name: 'Elastic Agent',
+            value: 'elastic-agent'
         },
         {
-            name: "Filebeat",
-            value: "filebeat"
+            name: 'Auditbeat',
+            value: 'auditbeat'
         },
         {
-            name: "Functionbeat",
-            value: "functionbeat"
+            name: 'Filebeat',
+            value: 'filebeat'
         },
         {
-            name: "Heartbeat",
-            value: "heartbeat"
+            name: 'Functionbeat',
+            value: 'functionbeat'
         },
         {
-            name: "Journalbeat",
-            value: "jounalbeat"
+            name: 'Heartbeat',
+            value: 'heartbeat'
         },
         {
-            name: "Metricbeat",
-            value: "metricbeat"
+            name: 'Journalbeat',
+            value: 'jounalbeat'
         },
         {
-            name: "Packetbeat",
-            value: "packetbeat"
+            name: 'Metricbeat',
+            value: 'metricbeat'
         },
         {
-            name: "Topbeat",
-            value: "topbeat"
+            name: 'Packetbeat',
+            value: 'packetbeat'
         },
         {
-            name: "Winlogbeat",
-            value: "winlogbeat"
+            name: 'Topbeat',
+            value: 'topbeat'
+        },
+        {
+            name: 'Winlogbeat',
+            value: 'winlogbeat'
         }
     ],
     default: 'elasticsearch',
 },
 {
     name: 'version',
-    type: 'input',
+    type: 'search-list',
     message: 'Type a version:',
-    default: function (value) {
-        (answers) =>  axios.get(`https://api.github.com/repos/elastic/${answers.product.toLowerCase()}/tags`)
-            .then(function (response) {
-                console.log(response.data[0].name.substring(1));
-            });
-    }
+    choices: (answers) =>  axios.get(`https://api.github.com/repos/elastic/${answers.product}/tags`)
+        .then( response => {
+            let versionsList = []
+            response.data.forEach( item => {
+                versionsList.push(item.name.substring(1));
+            })
+            return versionsList;
+        }).catch((err) => {
+            console.error(chalk.red("No version found :/"))
+            process.exit(0);
+        })
 },
 {
     name: 'confirm',
@@ -129,10 +138,10 @@ inquirer.prompt([
 
 
 // const options = yargs
-//  .usage("Usage: -p <product> -v <version> -a <architecture>")
-//  .option("p", { alias: "product", describe: "Elastic product name", type: "string", demandOption: true })
-//  .option("v", { alias: "version", describe: "Elastic product version", type: "string", demandOption: true })
-//  .option("a", { alias: "arch", describe: "Elastic product architecture", type: "string", demandOption: true })
+//  .usage('Usage: -p <product> -v <version> -a <architecture>')
+//  .option('p', { alias: 'product', describe: 'Elastic product name', type: 'string', demandOption: true })
+//  .option('v', { alias: 'version', describe: 'Elastic product version', type: 'string', demandOption: true })
+//  .option('a', { alias: 'arch', describe: 'Elastic product architecture', type: 'string', demandOption: true })
 //  .argv;
 
 // const dl = `Downloading ${options.product}...`;
